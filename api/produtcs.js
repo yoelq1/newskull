f7eb87721cca80a612057bbe495f677b23dbe7e4
yoelq1 <149291230+yoelq1@users.noreply.github.com>
2025-09-06 18:45:25 +0700

    Update produtcs.js

--- api/produtcs.js
+++ api/produtcs.js
@@ -4,16 +4,16 @@ import path from "path";
 const dataDir = path.join(process.cwd(), "data");
 const productsFile = path.join(dataDir, "products.json");
 
-export default function handler(req, res) {
-  if(req.method === "GET") {
+export default function handler(req, res){
+  if(req.method === "GET"){
     const products = JSON.parse(fs.readFileSync(productsFile));
     res.status(200).json(products);
-  } else if(req.method === "POST") {
+  } else if(req.method === "POST"){
     const body = JSON.parse(req.body);
     const products = JSON.parse(fs.readFileSync(productsFile));
     const newProduct = { id: Date.now(), ...body };
     products.push(newProduct);
-    fs.writeFileSync(productsFile, JSON.stringify(products, null, 2));
+    fs.writeFileSync(productsFile, JSON.stringify(products,null,2));
     res.status(200).json(newProduct);
   } else {
     res.status(405).send("Method Not Allowed");
