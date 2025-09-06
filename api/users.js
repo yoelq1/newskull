e58e923362f799439b4e98b4b463eddb4cb5e2c6
yoelq1 <149291230+yoelq1@users.noreply.github.com>
2025-09-06 18:37:30 +0700

    Create users.js

---
+++ api/users.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+import path from "path";
+
+const dataDir = path.join(process.cwd(), "data");
+const usersFile = path.join(dataDir, "users.json");
+
+export default function handler(req, res) {
+  if(req.method === "GET") {
+    const users = JSON.parse(fs.readFileSync(usersFile));
+    res.status(200).json(users);
+  } else if(req.method === "POST") {
+    const body = JSON.parse(req.body);
+    const users = JSON.parse(fs.readFileSync(usersFile));
+    const newUser = { id: Date.now(), ...body };
+    users.push(newUser);
+    fs.writeFileSync(usersFile, JSON.stringify(users, null, 2));
+    res.status(200).json(newUser);
+  } else {
+    res.status(405).send("Method Not Allowed");
+  }
+}
